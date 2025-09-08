506c8106cf7a1efcf272526828612d44133b0bb8
Yael Quintanilla Ramírez <153146490+YaelQR@users.noreply.github.com>
2025-09-07 20:14:24 -0600

    Update 03_query_faltas.sql

--- 03_query_faltas.sql
+++ 03_query_faltas.sql
@@ -1,6 +1,20 @@
-    SELECT g.periodo, g.seccion, g.nombre_grupo,
-           COUNT(*) FILTER (WHERE a.presente = FALSE) AS total_faltas
-    FROM asistencia a
-    JOIN grupos g ON a.periodo = g.periodo AND a.seccion = g.seccion
-    GROUP BY g.periodo, g.seccion, g.nombre_grupo
-    ORDER BY g.seccion;
+SELECT
+    g.periodo,
+    g.codigo_grupo,
+    m.nombre AS nombre_materia,
+    COUNT(a.asistio) FILTER (WHERE a.asistio = FALSE) AS total_faltas
+FROM
+    asistencia a
+JOIN
+    inscripcion i ON a.id_inscripcion = i.id_inscripcion
+JOIN
+    grupo g ON i.id_grupo = g.id_grupo
+JOIN
+    materia m ON g.codigo_materia = m.codigo
+GROUP BY
+    g.periodo,
+    g.codigo_grupo,
+    m.nombre
+ORDER BY
+    g.periodo,
+    g.codigo_grupo;
